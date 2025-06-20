db70d52493fe38d5ec90dfee34db98ba3ef5a8ee
JsanchezMGit <jesuh_aguilas@msn.com>
2025-06-19 21:11:16 -0600

    postgresql config

--- meli_backend/settings.py
+++ meli_backend/settings.py
@@ -72,16 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "meli_backend.wsgi.application"
 
+import psycopg2
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 # Password validation
